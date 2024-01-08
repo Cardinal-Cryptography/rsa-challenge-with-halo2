ee31db12e11304c7f0edc54b7323668b5a03014e
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-01-08 13:33:26 +0100

    RSA circuit

--- Makefile
+++ Makefile
@@ -2,16 +2,21 @@
 
 build: ## Build the project
 	cargo contract build --release --manifest-path ./rsa_contract/Cargo.toml
+	cargo build --release --manifest-path ./rsa_circuit/Cargo.toml
 
 lint: ## Run the linter
 	cargo +nightly fmt --manifest-path ./rsa_contract/Cargo.toml
 	cargo clippy --release --manifest-path ./rsa_contract/Cargo.toml -- -D warnings
+	cargo +nightly fmt --manifest-path ./rsa_circuit/Cargo.toml
+	cargo clippy --release --manifest-path ./rsa_circuit/Cargo.toml -- -D warnings
 
 test: ## Run tests
 	cargo test --release --manifest-path ./rsa_contract/Cargo.toml -- --show-output
+	cargo test --release --manifest-path ./rsa_circuit/Cargo.toml -- --show-output
 
 clean: ## Clean all the build files
 	cargo clean --manifest-path ./rsa_contract/Cargo.toml
+	cargo clean --manifest-path ./rsa_circuit/Cargo.toml
 
 help: ## Displays this help
 	@awk 'BEGIN {FS = ":.*##"; printf "Usage:\n  make \033[1;36m<target>\033[0m\n\nTargets:\n"} /^[a-zA-Z0-9_-]+:.*?##/ { printf "  \033[1;36m%-25s\033[0m %s\n", $$1, $$2 }' $(MAKEFILE_LIST)
---
+++ rsa_circuit/src/lib.rs
@@ -0,0 +1,55 @@
+//! Circuit for the RSA challenge.
+
+#![deny(missing_docs)]
+
+use halo2_proofs::{
+    circuit::{Layouter, Value},
+    halo2curves::bn256::Fr,
+    plonk::{Circuit, ConstraintSystem, Error},
+    standard_plonk::StandardPlonk,
+};
+
+/// Circuit representing the RSA challenge.
+///
+/// There are two advices (private inputs): `p`, and `q` and two instances (public inputs): `n` and `account`. The
+/// values should satisfy: `p * q = n`. The `account` instance is artificially included in the circuit to prevent
+/// front running attacks.
+///
+/// Since `account` is usually 256 bits long (`[u8 ;32]`), we need to split it into two 128 bit chunks, so that we have
+/// certainty that both can be safely decoded as `Fr` elements.
+#[derive(Default)]
+pub struct RsaChallenge {
+    /// First prime factor of the challenge.
+    p: Fr,
+    /// Second prime factor of the challenge.
+    q: Fr,
+}
+
+impl Circuit<Fr> for RsaChallenge {
+    type Config = <StandardPlonk as Circuit<Fr>>::Config;
+    type FloorPlanner = <StandardPlonk as Circuit<Fr>>::FloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        RsaChallenge::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<Fr>) -> Self::Config {
+        StandardPlonk::configure(meta)
+    }
+
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl Layouter<Fr>,
+    ) -> Result<(), Error> {
+        layouter.assign_region(
+            || "",
+            |mut region| {
+                region.assign_advice(|| "", config.a, 0, || Value::known(self.p))?;
+                region.assign_advice(|| "", config.b, 0, || Value::known(self.q))?;
+                region.assign_fixed(|| "", config.q_ab, 0, || Value::known(-Fr::one()))?;
+                Ok(())
+            },
+        )
+    }
+}
