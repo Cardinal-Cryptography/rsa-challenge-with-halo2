d00234461399706226be39a43580ac3b5873f0d3
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-01-08 23:43:24 +0100

    Circuit: proof generation and (de)serialization

--- rsa_circuit/src/tests.rs
+++ rsa_circuit/src/tests.rs
@@ -1,25 +1,12 @@
 use halo2_proofs::{
-    halo2curves::{
-        bn256::{Bn256, Fr, G1Affine},
-        ff::PrimeField,
-    },
-    plonk::{create_proof, verify_proof, Error, VerifyingKey},
-    poly::kzg::{
-        commitment::ParamsKZG,
-        multiopen::{ProverGWC, VerifierGWC},
-        strategy::SingleStrategy,
-    },
-    transcript::{
-        Blake2bRead, Blake2bWrite, Challenge255, TranscriptReadBuffer, TranscriptWriterBuffer,
-    },
+    halo2curves::bn256::{Bn256, Fr, G1Affine},
+    plonk::{verify_proof, Error, VerifyingKey},
+    poly::kzg::{commitment::ParamsKZG, multiopen::VerifierGWC, strategy::SingleStrategy},
+    transcript::{Blake2bRead, TranscriptReadBuffer},
     SerdeFormat,
 };
-use rand::rngs::OsRng;
 
-use crate::{
-    utils::{generate_setup, prepare_public_input, Setup},
-    RsaChallenge,
-};
+use crate::utils::{generate_proof, generate_setup, prepare_public_input, Setup, SubmissionData};
 
 const CIRCUIT_MAX_K: u32 = 5;
 const ACCOUNT: [u8; 32] = [0u8; 32];
@@ -32,33 +19,15 @@ struct TestSetup {
     params: ParamsKZG<Bn256>,
 }
 
-fn setup(p: u128, q: u128, account: [u8; 32]) -> TestSetup {
-    let Setup { pk, vk, params, .. } = generate_setup(CIRCUIT_MAX_K);
-
-    let n = p * q;
-    let circuit = RsaChallenge {
-        p: Some(Fr::from_u128(p)),
-        q: Some(Fr::from_u128(q)),
-    };
-    let instances = prepare_public_input(n, account);
-
-    let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
-    create_proof::<_, ProverGWC<'_, Bn256>, _, _, _, _>(
-        &params,
-        &pk,
-        &[circuit],
-        &[&[&instances]],
-        OsRng,
-        &mut transcript,
-    )
-    .expect("prover should not fail");
-    let proof = transcript.finalize();
+fn pipeline(p: u128, q: u128, account: [u8; 32]) -> TestSetup {
+    let setup = generate_setup(CIRCUIT_MAX_K);
+    let data = generate_proof(&setup, p, q, account);
 
     TestSetup {
-        proof,
-        instances,
-        vk,
-        params,
+        proof: data.proof,
+        instances: data.instances,
+        vk: setup.vk,
+        params: setup.params,
     }
 }
 
@@ -74,17 +43,17 @@ fn verify(setup: TestSetup) -> Result<(), Error> {
 
 #[test]
 fn accepts_correct_proof() {
-    assert!(verify(setup(41, 43, ACCOUNT)).is_ok());
+    assert!(verify(pipeline(41, 43, ACCOUNT)).is_ok());
 }
 
 #[test]
 fn works_with_big_numbers() {
-    assert!(verify(setup(7413101572609314289, 6786072055295288333, ACCOUNT)).is_ok());
+    assert!(verify(pipeline(7413101572609314289, 6786072055295288333, ACCOUNT)).is_ok());
 }
 
 #[test]
 fn does_not_accept_fake_account() {
-    let true_setup = setup(41, 43, ACCOUNT);
+    let true_setup = pipeline(41, 43, ACCOUNT);
     let fake_setup = TestSetup {
         instances: prepare_public_input(41 * 43, FAKE_ACCOUNT),
         ..true_setup
@@ -94,8 +63,8 @@ fn does_not_accept_fake_account() {
 
 #[test]
 fn does_not_accept_incorrect_proof() {
-    let true_setup = setup(41, 43, ACCOUNT);
-    let alternative_setup = setup(11, 13, ACCOUNT);
+    let true_setup = pipeline(41, 43, ACCOUNT);
+    let alternative_setup = pipeline(11, 13, ACCOUNT);
     let fake_setup = TestSetup {
         proof: alternative_setup.proof,
         ..true_setup
@@ -115,4 +84,14 @@ fn setup_serialization_works() {
         setup.pk.to_bytes(SerdeFormat::RawBytesUnchecked),
         deserialized.pk.to_bytes(SerdeFormat::RawBytesUnchecked)
     );
+}
+
+#[test]
+fn submission_data_serialization_works() {
+    let setup = generate_setup(CIRCUIT_MAX_K);
+    let data = generate_proof(&setup, 11, 13, ACCOUNT);
+    let serialized = data.clone().to_bytes();
+    let deserialized = SubmissionData::from_bytes(&mut serialized.as_slice());
+
+    assert_eq!(data, deserialized);
 }
--- rsa_circuit/src/utils.rs
+++ rsa_circuit/src/utils.rs
@@ -5,9 +5,13 @@ use halo2_proofs::{
         bn256::{Bn256, Fr, G1Affine},
         ff::PrimeField,
     },
-    plonk::{keygen_pk, keygen_vk, ProvingKey, VerifyingKey},
-    poly::{commitment::Params, kzg::commitment::ParamsKZG},
+    plonk::{create_proof, keygen_pk, keygen_vk, ProvingKey, VerifyingKey},
+    poly::{
+        commitment::Params,
+        kzg::{commitment::ParamsKZG, multiopen::ProverGWC},
+    },
     standard_plonk::StandardPlonk,
+    transcript::{Blake2bWrite, Challenge255, TranscriptWriterBuffer},
     SerdeFormat,
 };
 use rand::rngs::OsRng;
@@ -15,6 +19,9 @@ use rand::rngs::OsRng;
 use crate::RsaChallenge;
 
 const SERDE_FORMAT: SerdeFormat = SerdeFormat::RawBytesUnchecked;
+
+/// Type representing an identifier of the participant.
+pub type Account = [u8; 32];
 
 /// Initial setup artifacts including trusted setup, proving key and verifying key.
 #[derive(Clone, Debug)]
@@ -65,10 +72,69 @@ pub fn generate_setup(k: u32) -> Setup {
 }
 
 /// Convert the public input from human-readable form to the scalar array.
-pub fn prepare_public_input(n: u128, account: [u8; 32]) -> [Fr; 3] {
+pub fn prepare_public_input(n: u128, account: Account) -> [Fr; 3] {
     [
         Fr::from_u128(n),
         Fr::from_u128(u128::from_le_bytes(account[..16].try_into().unwrap())),
         Fr::from_u128(u128::from_le_bytes(account[16..].try_into().unwrap())),
     ]
+}
+
+/// Proof accompanied by the public input (instance).
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct SubmissionData {
+    /// SNARK proof.
+    pub proof: Vec<u8>,
+    /// Public input.
+    pub instances: [Fr; 3],
+}
+
+impl SubmissionData {
+    /// Serialize submission data to raw bytes.
+    pub fn to_bytes(self) -> Vec<u8> {
+        let mut buffer = vec![];
+        for i in self.instances {
+            buffer.extend(i.to_bytes());
+        }
+        buffer.extend(self.proof);
+        buffer
+    }
+
+    /// Deserialize submission data from raw bytes.
+    pub fn from_bytes(buffer: &mut &[u8]) -> Self {
+        let instances = [
+            Fr::from_bytes(buffer[..32].try_into().unwrap()).expect("Failed to read instance"),
+            Fr::from_bytes(buffer[32..64].try_into().unwrap()).expect("Failed to read instance"),
+            Fr::from_bytes(buffer[64..96].try_into().unwrap()).expect("Failed to read instance"),
+        ];
+
+        Self {
+            instances,
+            proof: buffer[96..].to_vec(),
+        }
+    }
+}
+
+/// Generate proof together with public input given `setup`, `p`, `q` and `account`.
+pub fn generate_proof(setup: &Setup, p: u128, q: u128, account: Account) -> SubmissionData {
+    let n = p * q;
+    let circuit = RsaChallenge {
+        p: Some(Fr::from_u128(p)),
+        q: Some(Fr::from_u128(q)),
+    };
+    let instances = prepare_public_input(n, account);
+
+    let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+    create_proof::<_, ProverGWC<'_, Bn256>, _, _, _, _>(
+        &setup.params,
+        &setup.pk,
+        &[circuit],
+        &[&[&instances]],
+        OsRng,
+        &mut transcript,
+    )
+    .expect("Failed to generate proof");
+    let proof = transcript.finalize();
+
+    SubmissionData { proof, instances }
 }
