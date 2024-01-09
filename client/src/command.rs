54df4e5cf84873bce3756bb69d8f3072b249d73a
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-01-09 12:38:11 +0100

    Client: generate and save submission data

--- client/src/command.rs
+++ client/src/command.rs
@@ -1,4 +1,8 @@
 #[derive(clap::Parser)]
 pub enum Command {
+    /// Run trusted setup phase and circuit-specific processing. Write the result (SRS, proving key, verifying key) to
+    /// a file.
     SetupSnark,
+    /// Generate a proof for the given advices and write it to a file.
+    GenerateProof { p: u128, q: u128 },
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,14 @@
+use std::fs::{read, write};
+
 use clap::Parser;
-use rsa_circuit::utils::generate_setup;
+use rsa_circuit::utils::{generate_proof, generate_setup, Account, Setup};
 
 use crate::command::Command;
 
 const CIRCUIT_MAX_K: u32 = 5;
+const SNARK_SETUP_FILE: &str = "snark-setup";
+const PROOF_FILE: &str = "submission-data";
+const ACCOUNT: Account = [0; 32];
 
 mod command;
 
@@ -11,6 +16,14 @@ fn main() {
     match Command::parse() {
         Command::SetupSnark => {
             let setup = generate_setup(CIRCUIT_MAX_K);
+            write(SNARK_SETUP_FILE, setup.to_bytes()).expect("Failed to save SNARK setup");
+        }
+        Command::GenerateProof { p, q } => {
+            let setup_serialized = read(SNARK_SETUP_FILE).expect("Failed to read SNARK setup");
+            let setup = Setup::from_bytes(&mut setup_serialized.as_slice());
+
+            let proof = generate_proof(&setup, p, q, ACCOUNT);
+            write(PROOF_FILE, proof.to_bytes()).expect("Failed to save SNARK proof");
         }
     }
 }
